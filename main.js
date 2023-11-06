94f1af53f82dbe9e06548d895d8510511464d46b
AdrikLarreategui <adrian15186@hotmail.com>
2023-11-06 12:08:51 +0100

    commit

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const getData = () => {
             users.forEach((item)=> {
             container.innerHTML += `<h1>${item.name}</h1>`
         }) 
-            const { data } = res //Imprimir por consola lista de usuarios y nombre de usuarios (solamente)
+            const { data } = res 
             console.log(data);
             data.forEach((item) => {
             console.log(item.name);
@@ -24,29 +24,3 @@ const getData = () => {
 const showGlobalVar = () => { console.log(users); } 
 getData();
 
-// const buttonData = document.querySelector('#getUsersGlobal')
-// const container = document.querySelector('#container');
-
-// let users = [];
-
-// const getData = () => {
-//   axios
-//     .get('https://jsonplaceholder.typicode.com/users')
-//     .then((res) => {
-//         users = res.data;
-//         //3. console.log(users);
-//      users.forEach((item)=> {
-//         container.innerHTML += `<h1>${item.name}</h1>`
-//       })
-//     //1 y 2.
-//       const { data } = res
-//       console.log(data);
-//       data.forEach((item) => {
-//       console.log(item.name);
-//       })
-//     })
-//     .catch((err) => console.error(err))
-// }
-
-// const mostrarVariableGlobal = () => { console.log(users); }
-// getData();
